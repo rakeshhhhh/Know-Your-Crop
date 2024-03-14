317dd6a73ada1baaab1cad16d06e9c2d399188ce
Rakesh Narayanan <r4rakesh000@gmail.com>
2024-03-14 18:16:04 +0530

    prediction  with user inputs Random Forest

--- kyc/kyc/train_model.py
+++ kyc/kyc/train_model.py
@@ -1,30 +1,30 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 import joblib
 
 # Load your dataset (replace 'path_to_your_dataset.csv' with the actual path)
 dataset = pd.read_csv('Crop_recommendation.csv')
+X = dataset.drop('label', axis=1)  # Features
+y = dataset['label']  # Target variable
 
-# Assuming your dataset contains features (temperature, humidity, soil_moisture, ph_value)
-# and a target variable (crop)
-# Extract features and target variable
-X = dataset.drop('label', axis=1)
-y = dataset['label']
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
+print(X)
+print(y)
 
-# Split dataset into training and testing sets
+# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = DecisionTreeClassifier()
+# Create a Random Forest Classifier
+model = RandomForestClassifier()
+
+# Train the model
 model.fit(X_train, y_train)
+
+# Make predictions on the test set
 y_pred = model.predict(X_test)
+
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Model Accuracy: {accuracy * 100:.2f}%')
 
-# Save the trained model to a file using joblib
-joblib.dump(model, 'trained_decision_tree_model.joblib')
-joblib.dump(label_encoder, 'decisiontree_encoder.pkl')
+joblib.dump(model, 'trained_random_forest_model.joblib')
--- kyc/myapp/views.py
+++ kyc/myapp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 from django.http import JsonResponse
-from sklearn.tree import DecisionTreeClassifier
+
 import numpy as np
 import joblib
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+
 
 
 def index(request):
@@ -19,32 +18,15 @@ def predict(request):
         moisture_level = float(request.POST['soil_moisture'])
         ph_value = float(request.POST['ph_value'])
 
-        # Example decision tree model
-        # X = np.array([[temperature, humidity, soil_moisture, ph_value]])
-        # Load your trained model here
-        model = joblib.load('kyc/trained_decision_tree_model.joblib')
-        label_encoder = joblib.load('kyc/decisiontree_encoder.pkl')
-        # predicted_crop = model.predict(X)
-        # Replace this with actual logic based on your trained model
-        user_input = pd.DataFrame([[temperature, ph_value, humidity, moisture_level]],
+        model = joblib.load('kyc/trained_random_forest_model.joblib')
+
+        user_input = pd.DataFrame([[temperature, humidity, ph_value, moisture_level]],
                                   columns=['temperature', 'humidity', 'ph', 'water availability'])
+
+        # Make prediction for user input
         predicted_crop = model.predict(user_input)
 
-        # predicted_crop = "Wheat"  # Placeholder
-        user_input_encoded = user_input.copy()
-        user_input_encoded['label'] = 0
-        user_input_encoded['label'] = label_encoder.transform(['rice'])[0]
-
-        predicted_crop_encoded = model.predict(user_input_encoded.drop('label', axis=1))
-
-        # Convert the predicted label back to the original class
-        predicted_crop = label_encoder.inverse_transform(predicted_crop_encoded)
-
         # print(f'\nThe predicted crop for the given soil factors is: {predicted_crop[0]}')
-
-    #     return HttpResponse(f"Predicted crop: {predicted_crop[0]}")
-    # else:
-    #     return HttpResponse("Invalid request")
 
         return JsonResponse({'predicted_crop': predicted_crop[0]})
     else:
